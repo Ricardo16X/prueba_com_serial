a89a167dd5cc61a2677810dffc93486194711197
Ricardo <perezricardo11@yahoo.com>
2019-10-08 17:42:48 -0600

    serial port

---
+++ prueba_puertoSerial/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace prueba_puertoSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.cmbPuertos = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 1;
+            this.serialPort1.PortName = "COM2";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.datosRecibidos);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(198, 173);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(12, 175);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(180, 20);
+            this.txtDato.TabIndex = 1;
+            // 
+            // cmbPuertos
+            // 
+            this.cmbPuertos.FormattingEnabled = true;
+            this.cmbPuertos.Location = new System.Drawing.Point(121, 80);
+            this.cmbPuertos.Name = "cmbPuertos";
+            this.cmbPuertos.Size = new System.Drawing.Size(121, 21);
+            this.cmbPuertos.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(248, 78);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Conectar";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 280);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbPuertos);
+            this.Controls.Add(this.txtDato);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.ComboBox cmbPuertos;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ prueba_puertoSerial/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace prueba_puertoSerial
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort puerto = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string item in SerialPort.GetPortNames())
+            {
+                cmbPuertos.Items.Add(item);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                puerto.BaudRate = 1;
+                puerto.DataBits = 8;
+                puerto.Parity = Parity.None;
+                puerto.DiscardNull = true;
+                puerto.StopBits = StopBits.One;
+                puerto.PortName = "COM1";
+                puerto.Open();
+                MessageBox.Show("Puerto abierto");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show(".");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] num = new byte[1];
+            try
+            {
+                num[0] = Convert.ToByte(Convert.ToInt32(txtDato.Text));
+                if (puerto.IsOpen)
+                {
+                    puerto.Write(num, 0, 1);
+                    MessageBox.Show(txtDato.Text);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+                throw;
+            }
+            
+        }
+
+        private void datosRecibidos(object sender, SerialDataReceivedEventArgs e)
+        {
+            string buffer = "";
+            buffer = puerto.ReadExisting();
+        }
+    }
+}
